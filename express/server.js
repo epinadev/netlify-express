37c907bd2d025bc62dd8f457eba1bc0080f5b9b2
Eduardo Piña <epinadev@gmail.com>
2020-04-18 23:27:15 -0500

    SSE fixes

--- express/server.js
+++ express/server.js
@@ -7,20 +7,23 @@ const { addNest, eventsHandler, getStatus } = require('./sse');
 const cors = require('cors');
 
 const app = express();
+const router = express.Router();
 
-// These was here
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>SSE Testing!</h1>');
+  res.end();
+});
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post('/nest', addNest);
+router.get('/events', eventsHandler);
+router.get('/status', getStatus);
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+
 app.use(cors());
 app.use(bodyParser.json());
-app.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-})
-
-app.post('/nest', addNest);
-app.get('/events', eventsHandler);
-app.get('/status', getStatus);
-
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
